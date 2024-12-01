5492a3cd25d740e2c05f26dae782e1ae1c604661
lukasWastaken <l.leutgeb@students.htl-leonding.ac.at>
2024-12-01 21:12:19 +0100

    01.12.2024

--- UE25-ShoppingCart/obj/Debug/net8.0/UE25-ShoppingCart.AssemblyInfo.cs
+++ UE25-ShoppingCart/obj/Debug/net8.0/UE25-ShoppingCart.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UE25-ShoppingCart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+640aa20b8bd8ffefabbfbc9b996c1ad1294acea2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b429c4f14133e044925631f7f24b0471ccd2ed42")]
 [assembly: System.Reflection.AssemblyProductAttribute("UE25-ShoppingCart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UE25-ShoppingCart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- UE27-VocabTrainer/obj/Debug/net8.0/UE27-VocabTrainer.AssemblyInfo.cs
+++ UE27-VocabTrainer/obj/Debug/net8.0/UE27-VocabTrainer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UE27-VocabTrainer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78a918a6c862474113080fba2670304cbac2e6bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b429c4f14133e044925631f7f24b0471ccd2ed42")]
 [assembly: System.Reflection.AssemblyProductAttribute("UE27-VocabTrainer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UE27-VocabTrainer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
