581d3062a66c1a1440d7c5bf05ddea916a943288
lukasWastaken <l.leutgeb@students.htl-leonding.ac.at>
2024-12-15 15:32:57 +0100

    Update for 15.12.2024

--- UE32-logarithmus/Program.cs
+++ UE32-logarithmus/Program.cs
@@ -10,12 +10,12 @@
             Console.WriteLine("Enter a number: ");
             input = int.Parse(Console.ReadLine());
 
-            while(input != 0)
+            while(input >= 1) 
             {
                 input = input / 2;
                 count++;
             }
-            count--;
+
             Console.WriteLine("Amount that the number can be divided by 2: " + count);
         }
     }
--- UE33-LinearMotion/obj/Debug/net8.0/UE33-LinearMotion.AssemblyInfo.cs
+++ UE33-LinearMotion/obj/Debug/net8.0/UE33-LinearMotion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UE33-LinearMotion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba98dc1513a793bf4dbaf2fe38ee30ce1c7cb256")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04880cf5af1107bad98a589a6d38e0e1c124dacd")]
 [assembly: System.Reflection.AssemblyProductAttribute("UE33-LinearMotion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UE33-LinearMotion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
