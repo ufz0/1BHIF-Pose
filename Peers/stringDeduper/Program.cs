3c2af67f4083d11b2397a2abbe33c256ffa01f51
lukasWastaken <l.leutgeb@students.htl-leonding.ac.at>
2025-01-13 21:05:10 +0100

    Added peers task
    Not finished yet.

---
+++ Peers/stringDeduper/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace stringDeduper
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input;
+            string sum = "";
+
+
+            Console.WriteLine("Enter a string to remove duplicates from: ");
+            input = Console.ReadLine().ToLower();
+
+
+
+            foreach(char i in input)
+            {
+                if(!sum.Contains(i))
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
