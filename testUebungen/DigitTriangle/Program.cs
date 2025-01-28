755f0a4f72d9c7252446f30a70e2f2540a008405
lukasWastaken <l.leutgeb@students.htl-leonding.ac.at>
2025-01-28 09:10:31 +0100

    Update for test

---
+++ testUebungen/DigitTriangle/Program.cs
@@ -0,0 +1,31 @@
+﻿/*------------------------------------------------------
+    HTBLA-Leonding / Class: 1BHIF
+ *------------------------------------------------------
+    Lukas Leutgeb, 27.01.2025
+ *------------------------------------------------------
+    Description:
+        A program to print a digit triangle.
+ *------------------------------------------------------
+*/
+using System;
+
+
+namespace digitTriangle
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number = 6;
+
+            for(int i = 1; i < number; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ testUebungen/ascii/Program.cs
@@ -0,0 +1,51 @@
+﻿/*------------------------------------------------------
+    HTBLA-Leonding / Class: 1BHIF
+ *------------------------------------------------------
+    Lukas Leutgeb, 28.01.2025
+ *------------------------------------------------------
+    Description:
+        A little ascii program
+ *------------------------------------------------------
+*/
+
+
+using System;
+
+namespace ascii
+{
+    class program
+    {
+        static void Main()
+        {
+            char num = '1';
+
+            if(num >= '0' && num <= '9')
+            {
+                Console.WriteLine("The character is a number");
+            }
+            else
+            {
+                Console.WriteLine("The character is not a number");
+            }
+
+            if(num >= 'A' && num <= 'Z')
+            {
+                Console.WriteLine("The character is an uppercase letter");
+            }
+            else
+            {
+                Console.WriteLine("The character is not an uppercase letter");
+            }
+
+            if(num >= 'a' && num <= 'z')
+            {
+                Console.WriteLine("The character is a lowercase letter");
+            }
+            else
+            {
+                Console.WriteLine("The character is not a lowercase letter");
+            }
+
+        }
+    }
+}
