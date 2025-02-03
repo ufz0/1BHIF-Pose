6e60c333f8737cf2b8b5f41dbef887195f831221
lukasWastaken <l.leutgeb@students.htl-leonding.ac.at>
2025-02-03 14:19:07 +0100

    Update for 03.02.2025

---
+++ UE51-extractLowerCase/Program.cs
@@ -0,0 +1,37 @@
+﻿/*------------------------------------------------------
+    HTBLA-Leonding / Class: 1BHIF
+ *------------------------------------------------------
+    Lukas Leutgeb, 30.01.2025
+ *------------------------------------------------------
+    Description:
+        A simple program to extract lower case chars
+ *------------------------------------------------------
+*/
+
+using System;
+
+namespace lowerCaseExtractor
+{
+    class lowerCaseExtractor
+    {
+        static void Main()
+        {
+            string input = "HaLLO Welten!";
+            int startIndex = 1;
+            int length = 3;
+            string output = "";
+
+            for(int i = startIndex; i < input.Length; i++)
+            {
+                char current = input[i];
+
+                if(current >= 'a' && current <= 'z' && output.Length < length)
+                {
+                    output += current;
+                }
+            }
+            
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ UE52-Substring/Program.cs
@@ -0,0 +1,53 @@
+﻿/*------------------------------------------------------
+    HTBLA-Leonding / Class: 1BHIF
+ *------------------------------------------------------
+    Lukas Leutgeb, 01.02.2025
+ *------------------------------------------------------
+    Description:
+        A program that extracts a specific string from another
+ *------------------------------------------------------
+*/
+
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace subString
+{
+    class subString
+    {
+        static void Main()
+        {
+            string input="";
+            int inputLen;
+
+            int subStart;
+            int subEnd;
+
+            string output="";
+
+            Console.WriteLine("Extract lower case characters\n=============================");
+            Console.WriteLine("Text eingeben:");
+            input = Console.ReadLine() ?? string.Empty;
+            inputLen = input.Length;
+            do 
+            {
+                Console.WriteLine("Please enter a substring start index: ");
+                subStart = int.Parse(Console.ReadLine() ?? string.Empty);
+
+                Console.WriteLine("Please enter a substring end index: ");
+                subEnd = int.Parse(Console.ReadLine() ?? string.Empty);
+
+                if(subStart < 0 || subEnd >= inputLen || subEnd < subStart)
+                {
+                    Console.WriteLine("Invalid numbers entered\n");
+                }
+            } while(subStart < 0 || subEnd > inputLen || subEnd < subStart);
+
+            for(int i = subStart; i< subEnd; i++)
+            {
+                output += input[i];
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
